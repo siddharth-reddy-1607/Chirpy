a26ff13b4fea7ca66d360a36184463fe3a762c87
siddharth-reddy-1607 <sidd.reddy1607@gmail.com>
2024-06-19 12:02:40 +0530

    Deeper understanding of Handlers and HandleFunc. Middleware and execution flow when we use Middleware. Routing and Patterns.

--- main.go
+++ main.go
@@ -1,17 +1,55 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
+
 type healthzHandler struct{}
 
-func (hh healthzHandler) ServeHTTP(w http.ResponseWriter,_ *http.Request){
+func (hh *healthzHandler) ServeHTTP(w http.ResponseWriter,_ *http.Request){
     w.WriteHeader(http.StatusOK)
     w.Header().Add("Content","text/plain")
     w.Header().Add("charset","utf-8")
     w.Write([]byte("OK"))
+}
+
+type PageMetrics struct{
+    timesVisted int
+}
+func (pm *PageMetrics) middlewareUpdateHomePageMetrics (next http.Handler) http.Handler{
+    icr := func(w http.ResponseWriter, r *http.Request){
+        pm.timesVisted += 1
+        next.ServeHTTP(w,r)
+    }
+    return http.HandlerFunc(icr)
+}
+func (pm *PageMetrics) middlewareResetHomePageMetrics(next http.Handler) http.Handler{
+    reset := func(w http.ResponseWriter,r *http.Request){
+        pm.timesVisted = 0
+        next.ServeHTTP(w,r)
+    }
+    return http.HandlerFunc(reset)
+}
+func (pm *PageMetrics) displayMetrics() http.Handler{
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+        hits := fmt.Sprintf("Hits: %d",pm.timesVisted)
+        w.Write([]byte(hits))
+    })
+}
+func (pm *PageMetrics) resetMetrics() http.Handler{
+    return http.HandlerFunc(func (w http.ResponseWriter,r *http.Request){
+        pm.timesVisted = 0
+    })
+}
+
+func addFSHeader(h http.Handler) http.Handler{
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+        w.Header().Add("Cache-Control","no-cache")
+        h.ServeHTTP(w,r)
+    })
 }
 
 func main(){
@@ -22,8 +60,11 @@ func main(){
     }
     fileSeverRoot := "."
     fileServerHander := http.FileServer(http.Dir(fileSeverRoot))
-    mux.Handle("/app/*",http.StripPrefix("/app/", fileServerHander))
-    mux.Handle("/healthz",healthzHandler{})
+    pm := PageMetrics{timesVisted : 0}
+    mux.Handle("/app/*",addFSHeader(pm.middlewareUpdateHomePageMetrics(http.StripPrefix("/app", fileServerHander))))
+    mux.Handle("GET /healthz",&healthzHandler{})
+    mux.Handle("GET /metrics",pm.displayMetrics())
+    mux.Handle("/reset",pm.resetMetrics())
     log.Println("Listening on 8080")
     log.Fatal(Server.ListenAndServe())
 }
